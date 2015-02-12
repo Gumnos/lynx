7203eb36a50e81dd2eb30099ecc69efaac1357bb
Tim Chase <git@tim.thechases.com>
2015-02-12 06:39:15 -0600

    lynx2.8.7pre.4

--- userdefs.h
+++ userdefs.h
@@ -1420,11 +1420,11 @@
  * the version definition with the Project Version on checkout.  Just
  * ignore it. - kw */
 /* $Format: "#define LYNX_VERSION \"$ProjectVersion$\""$ */
-#define LYNX_VERSION "2.8.7pre.3"
+#define LYNX_VERSION "2.8.7pre.4"
 #define LYNX_WWW_HOME "http://lynx.isc.org/"
 #define LYNX_WWW_DIST "http://lynx.isc.org/current/"
 /* $Format: "#define LYNX_DATE \"$ProjectDate$\""$ */
-#define LYNX_DATE "Mon, 25 May 2009 14:05:31 -0700"
+#define LYNX_DATE "Mon, 25 May 2009 17:10:12 -0700"
 #define LYNX_DATE_OFF 5		/* truncate the automatically-generated date */
 #define LYNX_DATE_LEN 11	/* truncate the automatically-generated date */
 
